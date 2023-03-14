9691a44078629f5cb3aa055253e86160ad1fc273
Ekaterina <ekhomiakov@yandex.ru>
2023-03-15 02:08:16 +0300

    Задача 13

--- Задача 13/Program.cs
+++ Задача 13/Program.cs
@@ -1,23 +1,15 @@
-﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели,
-// и проверяет, является ли этот день выходным.
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
 
-Console.Clear();
-Console.Write("Введите число от 1 до 7 ");
-int dayNumber = Convert.ToInt32(Console.ReadLine());
-
-if (dayNumber == 1)
-  Console.WriteLine("Понедельник будний день");
-else if (dayNumber == 2)
- Console.WriteLine("Вторник будний день");
-  else if (dayNumber == 3)
-  Console.WriteLine("Среда будний день");
-  else if (dayNumber == 4)
-  Console.WriteLine("Четверг будний день");
-  else if (dayNumber == 5)
-  Console.WriteLine("Пятница будний день");
-  else if (dayNumber == 6)
-  Console.WriteLine("Суббота выходной день");
-  else if (dayNumber == 7)
-  Console.WriteLine("Воскресенье выходной день");
-  else 
-  Console.WriteLine("Вы ввели число не от 1 до 7");
+Console.Write("Введи число: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+if (anyNumberText.Length > 2){
+  Console.WriteLine("третья цифра -> " + anyNumberText[2]);
+}
+else {
+  Console.WriteLine("-> третьей цифры нет");
+}
